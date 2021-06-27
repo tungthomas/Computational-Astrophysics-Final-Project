513811d5347e87ba613ef74a6cfd9602f1c1449f
tungthomas <46117730+tungthomas@users.noreply.github.com>
2021-06-27 20:07:09 +0800

    main function

--- pm_no_allocation.c
+++ pm_no_allocation.c
@@ -1,5 +1,3 @@
-// 第二部分的code，尚未平行
-
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,41 +5,59 @@
 
 #define pi acos(-1)
 
-// Choose the calculation type
+// Choose the deposition type
 #define NGP 1
 #define CLC 2
 #define TSC 3
-#define type NGP
+#define type TSC
+
+// Choose the integration type
+#define KDK 1
+#define DKD 2
+#define type_int KDK
 
 const double L = 1.0; // 1-D computational domain size
 const int N = 5;      // number of equally spaced sampling points
-const int np = 10;    // # of particles
+const int np = 1;     // # of particles        MUSt BE SET AT FIRST
 const int G = 1;      //gravitational const
 
+int step = 0; // record # of steps
 double t = 0.0;
 double dt = 0.01; // Time scale
+
+// determine the runing step or total time. You can choose one of them to set
+int total_step = 100;
+double tf = 1; //end time
 
 // grid length
 double dx = L / N;
 // Define functions
 
 void linspace(float xi, float xf, int n, double func[]);
-void csv_converter(char name[], double arr[np][3], double m[np]);
+void csv_converter(char name[], double p[np][3], double pv[np][3], double m[np]);
 void set_Narray0_3d(double arr[N][N][N]);
 void NGP_deposition(double p[np][3], double pm[np], double *g[3], double g_m[N][N][N], int g_sp[np][3], double g_w[np][type][type][type]);
 void CLC_deposition(double p[np][3], double pm[np], double *g[3], double g_m[N][N][N], int g_sp[np][3], double g_w[np][type][type][type]);
 void TSC_deposition(double p[np][3], double pm[np], double *g[3], double g_m[N][N][N], int g_sp[np][3], double g_w[np][type][type][type]);
-void force_interpolation(double p[np][3], double pm[np], double *g[3], double g_f[N][N][N][3], int g_sp[np][3], double g_w[np][type][type][type], double a[np][3]);
+void force_interpolation(double p[np][3], double pm[np], double g_f[N][N][N][3], int g_sp[np][3], double g_w[np][type][type][type], double a[np][3]);
+void K_move(double a[np][3], double pv[np][3], double n);
+void D_move(double p[np][3], double pv[np][3], double n);
+void get_acc(double p[np][3], double pm[np], double *g[3], double a[np][3]);
+void set_3d_vec0(double arr[N][N][N][3]);
+// The following functions are just for testing
+void direct_N(double p[np][3], double pm[np], double a[np][3]);
+void direct_N_mesh(double g_m[N][N][N], double *g[3], double g_a[N][N][N][3]);
 
 int main(void)
 {
     // Store particle position
     double pos[np][3];
+    double velocity[np][3];
     double mass[np];
 
     // Input the position of particles from csv file
     char filename[] = "test.csv";
-    csv_converter(filename, pos, mass);
+    csv_converter(filename, pos, velocity, mass);
 
     // // check if "pos" and "mass" is OK~
     // for (int i = 0; i < np; i++)
@@ -60,70 +76,29 @@ int main(void)
     linspace(0.0, L, N, x);
     linspace(0.0, L, N, y);
     linspace(0.0, L, N, z);
-
-    // initialize some storage array
-    double grid_mass[N][N][N];                // Store grid mass value
-    set_Narray0_3d(grid_mass);                // Set all # to 0
-    int grid_start_pos[np][3];                // The smallest grid id
-    double grid_weight[np][type][type][type]; // It's a density function. NGP use 1. CLC use 8. TSC use 27. The order is the same with three for loop from x to z
-
     double *grid[3] = {x, y, z}; //  Use to load the grid position
 
-    // Deposite particle
-    if (type == NGP)
+    while (t < tf || step < total_step)
     {
-        NGP_deposition(pos, mass, grid, grid_mass, grid_start_pos, grid_weight);
+        double acc[np][3];
+        set_particle_param0(acc);
+        if (type_int == KDK)
+        {
+            get_acc(pos, mass, grid, acc);
+            K_move(acc, velocity, 0.5);
+            D_move(pos, velocity, 1);
+            set_particle_param0(acc);
+            get_acc(pos, mass, grid, acc);
+            K_move(acc, velocity, 0.5);
+        }
+        if (type_int == DKD)
+        {
+            D_move(pos, velocity, 0.5);
+            get_acc(pos, mass, grid, acc);
+            K_move(acc, velocity, 1);
+            D_move(pos, velocity, 0.5);
+        }
     }
-    if (type == CLC)
-    {
-        CLC_deposition(pos, mass, grid, grid_mass, grid_start_pos, grid_weight);
-    }
-    if (type == TSC)
-    {
-        TSC_deposition(pos, mass, grid, grid_mass, grid_start_pos, grid_weight);
-    }
-    // // check grid mass
-    // double count = 0.0;
-    // for (int i = 0; i < N; i++)
-    // {
-    //     // int p_pos[3]; // To store the grid number of the current particle
-    //     for (int j = 0; j < N; j++)
-    //     {
-    //         for (int k = 0; k < N; k++)
-    //         {
-    //             printf("%1.1f  ", grid_mass[i][j][k]);
-    //             count += grid_mass[i][j][k];
-    //         }
-    //     }
-    // }
-    // printf("\nTotal mass is %f", count);
-
-    // Calculate the gravitational force with Poisson solver
-    //~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~
-    //~~~~~~~~~~~~~
-
-    // Let the result force is
-    double grid_force[N][N][N][3];
-
-    // Accelaration for each particle
-    double acc[np][3];
-
-    //  Interpolation
-    force_interpolation(pos, mass, grid, grid_force, grid_start_pos, grid_weight, acc);
-
-    // if (type == CLC)
-    // {
-
-    // }
-    // if (type == TSC)
-    // {
-
-    // }
-
     return 0;
 }
 
@@ -138,14 +113,14 @@ void linspace(float xi, float xf, int n, double func[])
     }
 }
 
-void csv_converter(char name[], double arr[np][3], double m[np])
+void csv_converter(char name[], double p[np][3], double pv[np][3], double m[np])
 {
 
     char table[np + 1][5][15];
 
     FILE *fp = fopen(name, "r"); // fp指向文件头部
     for (int i = 0; i < np + 1; i++)
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < 8; j++)
         {
             fscanf(fp, "%[^,\n]", table[i][j]); // 读到逗号或\n为止
             fgetc(fp);                          // 读取一个字符(逗号或\n)
@@ -156,19 +131,31 @@ void csv_converter(char name[], double arr[np][3], double m[np])
 
     for (int i = 1; i < np + 1; i++)
     {
-        for (int j = 1; j < 5; j++)
+        for (int j = 1; j < 8; j++)
         {
             if (j == 1)
             {
                 char *c = table[i][j];
                 double number = strtod(c, NULL);
                 m[i - 1] = number;
-                continue;
+                // printf("%f ", number);
             }
-            char *c = table[i][j];
-            double number = strtod(c, NULL);
-            arr[i - 1][j - 2] = number;
+            else if (j == 2 || j == 3 || j == 4)
+            {
+                char *c = table[i][j];
+                double number = strtod(c, NULL);
+                p[i - 1][j - 2] = number;
+                // printf("%f ", number);
+            }
+            else
+            {
+                char *c = table[i][j];
+                double number = strtod(c, NULL);
+                pv[i - 1][j - 5] = number;
+                // printf("%f ", number);
+            }
         }
+        printf("\n");
     }
 }
 
@@ -182,6 +169,17 @@ void set_Narray0_3d(double arr[N][N][N])
             {
                 arr[i][j][k] = 0.0;
             }
+        }
+    }
+}
+
+void set_particle_param0(double arr[np][3])
+{
+    for (int i = 0; i < np; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            arr[i][j] = 0.0;
         }
     }
 }
@@ -413,7 +411,7 @@ void TSC_deposition(double p[np][3], double pm[np], double *g[3], double g_m[N][
                 {
                     g_w[i][xn][yn][zn] = dist[0][xn] * dist[1][yn] * dist[2][zn];
                     // printf("(%d, %d, %d, %f)\n", xn, yn, zn, g_w[i][xn][yn][zn]);
-                    count += g_w[i][xn][yn][zn];
+                    // count += g_w[i][xn][yn][zn];
                 }
             }
             // printf("\n");
@@ -428,21 +426,24 @@ void TSC_deposition(double p[np][3], double pm[np], double *g[3], double g_m[N][
                 for (int zn = 0; zn < 3; zn++)
                 {
                     g_m[p_pos[0] + xn][p_pos[1] + yn][p_pos[2] + zn] += pm[i] * g_w[i][xn][yn][zn];
+                    count += pm[i] * g_w[i][xn][yn][zn];
+                    printf("%f ", pm[i] * g_w[i][xn][yn][zn]);
                 }
             }
         }
+        printf("\nCount = %f\n", count);
 
         // check if pos is correct
-        printf("%d.  ", i + 1);
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%f -> %f, ", p[i][j], g[j][p_pos[j]]);
-        }
-        printf("\n");
+        // printf("%d.  ", i + 1);
+        // for (int j = 0; j < 3; j++)
+        // {
+        //     printf("%f -> %f, ", p[i][j], g[j][p_pos[j]]);
+        // }
+        // printf("\n");
     }
 }
 
-void force_interpolation(double p[np][3], double pm[np], double *g[3], double g_f[N][N][N][3], int g_sp[np][3], double g_w[np][type][type][type], double a[np][3])
+void force_interpolation(double p[np][3], double pm[np], double g_f[N][N][N][3], int g_sp[np][3], double g_w[np][type][type][type], double a[np][3])
 {
     for (int i = 0; i < np; i++)
     {
@@ -455,6 +456,136 @@ void force_interpolation(double p[np][3], double pm[np], double *g[3], double g_
                     for (int cor = 0; cor < 3; cor++)
                     {
                         a[i][cor] = g_f[g_sp[i][0] + xn][g_sp[i][1] + yn][g_sp[i][2] + zn][cor] * g_w[i][xn][yn][zn] / pm[i];
+                    }
+                }
+            }
+        }
+    }
+}
+
+// orbit integration
+
+void K_move(double a[np][3], double pv[np][3], double n)
+{
+    for (int i = 0; i < np; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            pv[i][j] += a[i][j] * n * dt;
+        }
+    }
+}
+
+void D_move(double p[np][3], double pv[np][3], double n)
+{
+    for (int i = 0; i < np; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            p[i][j] += pv[i][j] * n * dt;
+        }
+    }
+}
+
+void get_acc(double p[np][3], double pm[np], double *g[3], double a[np][3])
+{
+
+    // initialize some storage array
+    double grid_mass[N][N][N];                // Store grid mass value
+    set_Narray0_3d(grid_mass);                // Set all # to 0
+    int grid_start_pos[np][3];                // The smallest grid id
+    double grid_weight[np][type][type][type]; // It's a density function. NGP use 1. CLC use 8. TSC use 27. The order is the same with three for loop from x to z
+
+    // Let the result force is (That is the result from Poisson solver)
+    double grid_acc[N][N][N][3];
+    set_3d_vec0(grid_acc);
+
+    // Deposite particle
+    if (type == NGP)
+    {
+        NGP_deposition(p, pm, g, grid_mass, grid_start_pos, grid_weight);
+    }
+    if (type == CLC)
+    {
+        CLC_deposition(p, pm, g, grid_mass, grid_start_pos, grid_weight);
+    }
+    if (type == TSC)
+    {
+        TSC_deposition(p, pm, g, grid_mass, grid_start_pos, grid_weight);
+    }
+
+    // Calculate the gravitational force with Poisson solver
+
+    // Use direct N-body to calculate the gravitational force for comparison
+
+    direct_N_mesh(grid_mass, g, grid_acc);
+
+    //  force interpolation for all particles 加權(?
+    force_interpolation(p, pm, grid_acc, grid_start_pos, grid_weight, a);
+}
+
+void set_3d_vec0(double arr[N][N][N][3])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                for (int cor = 0; cor < N; cor++)
+                {
+                    arr[i][j][k][cor] = 0.0;
+                }
+            }
+        }
+    }
+}
+
+//
+// The following functions are just for testing.
+//
+// If we need to use the direct N-body to calculate the self-gravity on each partticle
+void direct_N(double p[np][3], double pm[np], double a[np][3])
+{
+    for (int i = 0; i < np; i++)
+    {
+        for (int j = 0; j < np; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            for (int cor = 0; cor < 3; cor++)
+            {
+                a[i][cor] += G * pm[j] * (p[j][cor] - p[i][cor]) / pow(fabs(p[j][cor] - p[i][cor]), 3);
+            }
+        }
+    }
+}
+
+// Use direct N-body to calculate the self gravity on mesh
+void direct_N_mesh(double g_m[N][N][N], double *g[3], double g_a[N][N][N][3])
+{
+    for (int x1 = 0; x1 < N; x1++)
+    {
+        for (int y1 = 0; y1 < N; y1++)
+        {
+            for (int z1 = 0; z1 < N; z1++)
+            {
+                for (int x2 = 0; x2 < N; x2++)
+                {
+                    for (int y2 = 0; y2 < N; y2++)
+                    {
+                        for (int z2 = 0; z2 < N; z2++)
+                        {
+                            if (x1 == x2 && y1 == y2 && z1 == z2)
+                            {
+                                continue;
+                            }
+                            g_a[x1][y1][z1][0] += G * g_m[x2][y2][z2] * (g[0][x2] - g[0][x1]) / pow(fabs(g[0][x2] - g[0][x1]), 3);
+                            g_a[x1][y1][z1][1] += G * g_m[x2][y2][z2] * (g[1][x2] - g[1][x1]) / pow(fabs(g[1][x2] - g[1][x1]), 3);
+                            g_a[x1][y1][z1][2] += G * g_m[x2][y2][z2] * (g[2][x2] - g[2][x1]) / pow(fabs(g[2][x2] - g[2][x1]), 3);
+                        }
                     }
                 }
             }
